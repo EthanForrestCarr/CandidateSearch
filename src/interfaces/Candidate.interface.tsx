3cb66fd3d71422020f3bc8622bd9cf25a56c4662
EthanForrestCarr <ethan@ethancarr.com>
2024-10-27 22:40:42 -0500

    structured candidate interface

---
+++ src/interfaces/Candidate.interface.tsx
@@ -0,0 +1,10 @@
+// TODO: Create an interface for the Candidate objects returned by the API
+interface Candidate {
+    name: string,
+    username: string,
+    location: any,
+    avatar: any,
+    email: string,
+    html_url: string,
+    company: string
+}
